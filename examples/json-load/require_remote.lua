bdfa7e0077a78f577689d404ef6f7b10ddf33648
Kendall Sutton <kspear@users.noreply.github.com>
2021-01-18 02:07:05 -0500

    Update require_remote.lua

--- examples/json-load/require_remote.lua
+++ examples/json-load/require_remote.lua
@@ -1,7 +1,7 @@
 -- require_remote.lua
 
 function getLibrary(url, filename)
-  local content = http.get(library_file)
+  local content = http.get(url)
   output = fs.open(filename, "w")
   
   output.write(content.readAll())
